cb10b1cbb395c10b2101f2482bd06053b80fa572
Niko411 <36671168+niko411@users.noreply.github.com>
2020-09-02 08:14:01 +0300

    Update pptpd.sh

--- pptpd.sh
+++ pptpd.sh
@@ -83,7 +83,7 @@ elif test $x -eq 2; then
     read p
  
 # get the VPS IP
-ip=`ifconfig venet0:0 | grep 'inet' | awk {'print $2'} | sed s/.*://`
+ip=`ifconfig eth0 | grep 'inet' -m 1 | awk {'print $2'} | sed s/.*://`
  
 # adding new user
 echo "$u    *   $p  *" >> /etc/ppp/chap-secrets
